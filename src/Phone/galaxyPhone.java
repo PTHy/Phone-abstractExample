2d86a1a8d8397a41b873acb9a740cc022507d1b8
PTHy <30455428+PTHy@users.noreply.github.com>
2018-04-27 15:08:07 +0900

    finish iphone and galaxy phone
    ye!

--- src/Phone/galaxyPhone.java
+++ src/Phone/galaxyPhone.java
@@ -1,5 +1,20 @@
 package Phone;
 
-public class galaxyPhone {
+public class galaxyPhone extends Phone{
+
+	@Override
+	public void turnOn() {
+		System.out.println("삼성로고 켜기");
+	}
+
+	@Override
+	public void turnOff() {
+		System.out.println("삼성로고 끄기");
+	}
+
+	@Override
+	public void internetSearch() {
+		System.out.println("크롬 인터넷 검색");
+	}
 
 }
--- src/Phone/iPhone.java
+++ src/Phone/iPhone.java
@@ -1,5 +1,20 @@
 package Phone;
 
-public class iPhone {
+public class iPhone extends Phone{
+
+	@Override
+	public void turnOn() {
+		System.out.println("애플 로고 켜기");
+	}
+
+	@Override
+	public void turnOff() {
+		System.out.println("애플 로고 끄기");
+	}
+
+	@Override
+	public void internetSearch() {
+		System.out.println("사파리 인터넷검색");
+	}
 
 }
