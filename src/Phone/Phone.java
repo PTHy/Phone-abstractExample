80ce5393c216b13a9f5235c75a438a0f680a60d0
PTHy <30455428+PTHy@users.noreply.github.com>
2018-04-27 15:06:11 +0900

    finish phone
    abstract class phone

--- src/Phone/Phone.java
+++ src/Phone/Phone.java
@@ -1,5 +1,10 @@
 package Phone;
 
-public class Phone {
-
+public abstract class Phone {
+	public String owner;
+	public Phone(String owner) {this.owner = owner;}
+	public Phone() {}
+	public abstract void turnOn();
+	public abstract void turnOff();
+	public abstract void internetSearch();
 }
